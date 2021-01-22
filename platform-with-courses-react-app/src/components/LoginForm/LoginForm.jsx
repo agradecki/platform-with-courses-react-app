85504992a9f0918af47ba37cff78dbbc9b9af0d0
Alan <alangradecki@wp.pl>
2021-01-22 23:11:11 +0100

    Added reset of inputs

--- platform-with-courses-react-app/src/components/LoginForm/LoginForm.jsx
+++ platform-with-courses-react-app/src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import bemCssModules from "bem-css-modules";
 
 import Modal from "../Modal/Modal";
@@ -41,6 +41,12 @@ const LoginForm = ({ handleOnClose, isModalOpen }) => {
       setValidateMessage(data.message);
     }
   };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      resetStateOfInputs();
+    }
+  }, [isModalOpen]);
 
   const validateMessageComponent = validateMessage.length ? (
     <p className={style("validate-message")}>{validateMessage}</p>
