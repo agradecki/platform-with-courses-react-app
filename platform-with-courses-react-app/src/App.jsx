b8ff1039a62fdbc568e27ca280907dc8769efc81
Alan <alangradecki@wp.pl>
2021-01-24 00:04:01 +0100

    Added support for optional chaining and navigation

--- platform-with-courses-react-app/src/App.jsx
+++ platform-with-courses-react-app/src/App.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { HashRouter as Router } from "react-router-dom";
 
 import Header from "./components/Header/Header";
+import AsideMenu from "./components/AsideMenu/AsideMenu";
+
 import StoreProvider from "./store/StoreProvider";
 
 import "./App.scss";
--- platform-with-courses-react-app/src/components/AsideMenu/AsideMenu.jsx
+++ platform-with-courses-react-app/src/components/AsideMenu/AsideMenu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import bemCssModules from "bem-css-modules";
 
 import AdminMenu from "./subcomponents/AdminMenu";
