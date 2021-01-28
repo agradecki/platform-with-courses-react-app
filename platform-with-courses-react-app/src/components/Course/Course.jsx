c874cd0c6339dd38cc6a2079d2962ebd66618890
Alan <alangradecki@wp.pl>
2021-01-28 20:11:15 +0100

    Lack of product title and change css style

--- platform-with-courses-react-app/src/components/Course/Course.jsx
+++ platform-with-courses-react-app/src/components/Course/Course.jsx
@@ -36,7 +36,7 @@ const Course = ({ authors, id, isUserContext = false, img, price, title }) => {
   return (
     <li>
       <article className={style()}>
-        <h3 className={style("title")}></h3>
+        <h3 className={style("title")}>{title}</h3>
         <img alt={title} className={style("image")} src={img} />
         <p className={style("price")}>{`Koszt kursu: ${price}zł`}</p>
         <p className={style("authors")}>{`Autorzy kursu: ${allAuthors}`}</p>
